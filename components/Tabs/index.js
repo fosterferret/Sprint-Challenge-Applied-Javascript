cebc3b312b07dd845e09188b8f43472f08ca1a14
Francis Bulus <notchera@gmail.com>
2019-09-06 08:36:12 +0100

    created a GET request with axios for lambda-times backend

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -8,6 +8,16 @@
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
 
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+  .then(response => {
+    response.data.topics.forEach(topic => {
+        topicsDiv.appendChild(tabMaker(topic))}
+    )
+  })
+  .catch(error => {
+    console.log(error);
+  })
+
 
 function tabMaker (topic) {
     const tabDiv = document.createElement('div');
